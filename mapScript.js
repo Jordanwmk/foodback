5a802dee5a0cdcb61cce719e14edbf24eb24345d
Jordan Wong <jwon223@UoA.auckland.ac.nz>
2016-02-02 10:40:50 +1300

    removed food labels on google maps

--- mapScript.js
+++ mapScript.js
@@ -37,18 +37,9 @@ function createMarker(place) {
 
 function removeLabels(){
 	var styles = [
-  {
-    stylers: [
-      { hue: "#00ffe6" },
-      { saturation: -20 }
-    ]
-  },{
+{
     featureType: "road",
     elementType: "geometry",
-    stylers: [
-      { lightness: 100 },
-      { visibility: "simplified" }
-    ]
   },{
     featureType: "road",
     elementType: "labels",
