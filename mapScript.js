2cb40ec89d3d05059ef24683f1191065dfafb45c
Jordan Wong <jwon223@UoA.auckland.ac.nz>
2016-02-02 10:32:26 +1300

    corrected syntax error in mapscript

--- mapScript.js
+++ mapScript.js
@@ -30,8 +30,8 @@ function createMarker(place) {
 	});
 
 	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(place.name);
-		infowindow.open(map, this);
+		infoWindow.setContent(place.name);
+		infoWindow.open(map, this);
 	});
 }
 
