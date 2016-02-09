44cde62fb97a981d0787c627188be26de8a79436
Jordan Wong <jwon223@UoA.auckland.ac.nz>
2016-02-10 11:46:00 +1300

    removed log off guest

--- head.js
+++ head.js
@@ -1,26 +1,13 @@
 $(document).ready(function(){
-	if (localStorage.getItem('foodbackLog')=='yes'){
-		//Logged in
-		//Script for inserting the side/navbar here
-		$.get("http://foodback3.azurewebsites.net/wrapper.html", function( data ) {
-			var element=$.parseHTML(data);
-			$('body').prepend(element);
+    //Logged in
+    //Script for inserting the side/navbar here
+    $.get("http://foodback3.azurewebsites.net/wrapper.html", function( data ) {
+        var element=$.parseHTML(data);
+        $('body').prepend(element);
 
-			var dom = $(data);
-			dom.filter('script').each(function(){
-				$.globalEval(this.text || this.textContent || this.innerHTML || '');
-			});
-		});
-	}else{
-		//Not logged in
-		$.get("http://foodback3.azurewebsites.net/wrapper2.html", function( data ) {
-			var element=$.parseHTML(data);
-			$('body').prepend(element);
-
-			var dom = $(data);
-			dom.filter('script').each(function(){
-				$.globalEval(this.text || this.textContent || this.innerHTML || '');
-			});
-		});
-	}
+        var dom = $(data);
+        dom.filter('script').each(function(){
+            $.globalEval(this.text || this.textContent || this.innerHTML || '');
+        });
+    });
 });
