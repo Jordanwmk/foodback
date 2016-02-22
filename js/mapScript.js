16a1765d396e1dbb0c0fa9f34d5cb2d2bb1b2c86
Jordan Wong <jwon223@UoA.auckland.ac.nz>
2016-02-23 10:45:31 +1300

    refactored files on vnearby

---
+++ js/mapScript.js
@@ -0,0 +1,109 @@
+var myLatLng = {lat: -36.8576017, lng: 174.7590294};
+var image = 'img/here.png';
+var supermarket = 'img/supermarket.png';
+var fruit = 'img/fruit.png';
+var market = 'img/market.png';
+var garden = 'img/garden.png';
+var convenience = 'img/convenience.png';
+var takeaway = 'img/takeaway.png';
+var other = 'img/other.png';
+var markerList = [supermarket, fruit, market, garden, convenience, takeaway, other];
+var zoomAmount = 18;
+var map, marker, infoWindow;
+
+function getFoodPlaces(){
+	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch({
+		location: myLatLng,
+		radius: 500,
+		types: ['bakery', 'bar' , 'cafe','food',
+		'grocery_or_supermarket', 'hospital', 'meal_delivery' , 'meal_takeaway',
+		'restaurant' , 'school']
+	}, createFoodMarker);
+}
+
+function createFoodMarker(results, status) {
+	if (status === google.maps.places.PlacesServiceStatus.OK) {
+		for (var i = 0; i < results.length; i++) {
+			createMarker(results[i]);
+		}
+	}
+} 
+
+function createMarker(place) {
+	var randomNumber = Math.floor(Math.random() * (markerList.length));
+	var tempImage = markerList[randomNumber];
+	
+	var marker = new google.maps.Marker({
+		position: place.geometry.location,
+		map: map,
+		icon: tempImage
+	});
+	var infoWindowContent = '<p style="padding:0px; margin:0px;" onclick=\"window.location.href=\'food.html\'\">' + place.name + '<br> 5 min&nbsp <i class="fa fa-bicycle"></i>' +
+							'<div class="row"><div class="col-xs-4" style="padding-right:0px;"><img style="width:50px; height:50px; padding:0px; margin:0px;" src="../img/oil2.png"/></div>' +
+							'<div class="col-xs-4" style="padding-right:0px;"><img style="width:50px; height:50px; padding:0px; margin:0px;" src="../img/salt2.png"/></div>' +
+							'<div class="col-xs-4"><img style="width:25px; height:25; padding:0px;  padding-top:25px; margin:0px;" src="../img/sugar2.png"/></div></div></p>';
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infoWindow.setContent(infoWindowContent);
+		infoWindow.open(map, this);
+	});
+}
+
+function initInfoWindow(){
+	infoWindow = new google.maps.InfoWindow({
+		map: map,
+		content: 'You are here'
+	});
+	infoWindow.open(map, marker);
+}
+
+function initMarker(){
+	marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		icon: image
+	});
+}
+
+function initMap() {
+
+	var styles = [
+		{
+			featureType: "poi",
+			elementType: "labels",
+			stylers: [
+				{ visibility: "off" }
+			]
+		}
+		,{
+			featureType: "poi.school",
+			elementType: "labels",
+			stylers: [
+				{ visibility: "on" }
+			]
+		}
+		,{
+			featureType: "poi.medical",
+			elementType: "labels",
+			stylers: [
+				{ visibility: "on" }
+			]
+		}
+	];
+
+	var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: zoomAmount,
+		center: myLatLng
+	});
+
+	//Associate the styled map with the MapTypeId and set it to display.
+	map.mapTypes.set('map_style', styledMap);
+	map.setMapTypeId('map_style');
+	
+	initMarker();
+	initInfoWindow();
+	getFoodPlaces();
+}
